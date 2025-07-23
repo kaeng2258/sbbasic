cf0e90963c7e51f902aecf99ab47a308705d4992
깃íkaeng2258 <kaeng2258@gmail.com>
2025-07-23 16:12:14 +0900

    calc

--- src/main/java/com/std/sbb/Homecontroller.java
+++ src/main/java/com/std/sbb/Homecontroller.java
@@ -2,6 +2,7 @@ package com.std.sbb;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -28,5 +29,28 @@ public class Homecontroller {
     @ResponseBody
     public int ShowMain4(){
         return i++;
+    }
+    @GetMapping("/home/plus")
+    @ResponseBody
+    public int ShowMain5(@RequestParam(value = "a", defaultValue = "0")int a, @RequestParam(value = "b", defaultValue = "0")int b){
+        return a+b;
+    }
+    @GetMapping("/home/calculate")
+    @ResponseBody
+    public int ShowMain6(@RequestParam(value = "a", defaultValue = "0")int a, @RequestParam(value = "b", defaultValue = "0")String b,@RequestParam(value = "c", defaultValue = "0")int c){
+        int d = 0;
+        if(b.equals("+")){
+            d = a+c;
+        }
+        else if(b.equals("-")){
+            d = a-c;
+        }
+        else if(b.equals("*")){
+            d = a*c;
+        }
+        else if (b.equals("/")){
+            d = a/c;
+        }
+        return d;
     }
 }
