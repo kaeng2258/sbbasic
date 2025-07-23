b4ce78c6612876c9e48466192a47e0525fb9eede
깃íkaeng2258 <kaeng2258@gmail.com>
2025-07-23 15:29:20 +0900

    num++

--- src/main/java/com/std/sbb/Homecontroller.java
+++ src/main/java/com/std/sbb/Homecontroller.java
@@ -6,10 +6,27 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class Homecontroller {
-
+    int i = 0;
     @GetMapping("/home/main")
     @ResponseBody
     public String ShowMain(){
         return "hi";
+    }
+
+    @GetMapping("/home/main2")
+    @ResponseBody
+    public String ShowMain2(){
+        return "bye";
+    }
+
+    @GetMapping("/home/main3")
+    @ResponseBody
+    public String ShowMain3(){
+        return "sry";
+    }
+    @GetMapping("/home/increase")
+    @ResponseBody
+    public int ShowMain4(){
+        return i++;
     }
 }
